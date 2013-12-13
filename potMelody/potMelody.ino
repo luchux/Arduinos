4bb1ad7436d5eb9aa668a4669a0e657e808159a6
luchux <lucianoguasco@gmail.com>
2013-12-13 19:20:49 -0300

    Added new potentiometer on A1 to control tempo

--- potMelody/potMelody.ino
+++ potMelody/potMelody.ino
@@ -13,9 +13,15 @@
  #include "pitches.h"
 
 const int analogInPin = A0; //Analog input that potenciometer is attached to
+const int analogInPin2 = A1; 
+
+const int TEMPOS = 8;
 
 int sensorValue = 0; //value read from the potenciometer
 int noteValue = 0;   //value mapped from potenciometer to note scales. 
+
+int sensorValue2 = 0; //value read from the potenciometer
+int tempoValue = 0;   //value mapped from potenciometer to note scales. 
 
 //amount of notes 
 int indexNotes = 7;
@@ -34,25 +40,36 @@ void setup() {
   // iterate over the notes of the melody:
   sensorValue = analogRead(analogInPin);
   noteValue = map(sensorValue, 0, 1023, 0, indexNotes);
+  
+  sensorValue2 = analogRead(analogInPin2);
+  tempoValue = map(sensorValue2, 0, 1023, 0, TEMPOS);
+  
   Serial.begin(9600); 
   
   // print the results to the serial monitor:
-  Serial.print("sensor = " );                       
+  Serial.print("sensor 1 = " );                       
   Serial.print(sensorValue);      
-  Serial.print("\t noteh = ");      
+  Serial.print("\t note 1 = ");      
   Serial.println(noteValue);   
+  
+    // print the results to the serial monitor:
+  Serial.print("sensor 2 = " );                       
+  Serial.print(sensorValue2);      
+  Serial.print("\t note 2 = ");      
+  Serial.println(tempoValue);   
   
   //for (int thisNote = 0; thisNote < 8; thisNote++) {
 
     // to calculate the note duration, take one second 
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-  int noteDuration = 1000/noteDurations[noteValue];
-  tone(8, melody[noteValue],noteDuration);
+  
+  // noteValue2 will represent time of tone
+  tone(8, melody[noteValue], 1000/tempoValue);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
-  int pauseBetweenNotes = noteDuration * 1.30;
+  int pauseBetweenNotes = (1000/tempoValue) * 1.30;
   delay(pauseBetweenNotes);
     // stop the tone playing:
   noTone(8);
